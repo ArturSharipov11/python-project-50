4efc75dd0d7c57dd5e464fa7c81c776461903631
artur sharipov <sharipoffartur@yandex.ru>
2023-05-01 21:16:21 +0500

    dsf

--- gendiff/__init__.py
+++
@@ -1,5 +0,0 @@
-from gendiff.diff_format import generate_diff
-
-__all__ = (
-    'generate_diff',
-)
--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,16 +1,18 @@
-
-#!/usr/bin/env python
-
-
+#!/usr/bin/env python3
+from gendiff.eng import run_gendiff
 import argparse
+
 
 def main():
     parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
-    parser.add_argument("first_file")
-    parser.add_argument("second_file")
-    parser.add_argument("-f", "--format", help='set format of output')
-    args = parser.parse_args()
-    print(args)
+    parser.add_argument("-f", '--format', metavar='FORMAT', help="set format of output")
+    parser.add_argument("first_file", type=str)
+    parser.add_argument("second_file", type=str)
+    args = vars(parser.parse_args())
+    file1 = args['first_file']
+    file2 = args['second_file']
+    run_gendiff(file1, file2)
+
 
 if __name__ == '__main__':
     main()
