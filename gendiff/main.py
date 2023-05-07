609c205aec4f8c4a97868803143c2198a51377dc
artur sharipov <sharipoffartur@yandex.ru>
2023-05-07 23:12:47 +0500

    sf

--- gendiff/main.py
+++ gendiff/main.py
@@ -1,19 +1,16 @@
 from gendiff.cli import parse_args
 from gendiff.eng import generate_diff
-from gendiff.tools import normalize_file_name
-from gendiff.out_print import print_diff
+from gendiff.tools import get_file_content
+from gendiff.out_print import diff_str
 import json
 
 
 def gen_diff():
-    args = parse_args() 
-    file1 = '/Users/artursharipov/python-project-50/tests/files/file1.json'
-    file2 = '/Users/artursharipov/python-project-50/tests/files/file2.json'
-    with open(normalize_file_name(file1)) as _:
-        dict1 = json.load(_)
-    with open(normalize_file_name(file2)) as _:
-        dict2 = json.load(_)
-    print_diff(generate_diff(dict1, dict2))
+    args = parse_args()
+    dict1 = get_file_content(args.first_file)
+    dict2 = get_file_content(args.second_file)
+    diff = generate_diff(dict1, dict2)
+    return diff_str(diff)
 
 
 def main():
--- gendiff/tools.py
+++ gendiff/tools.py
@@ -1,5 +1,26 @@
 import os
 
 
-def normalize_file_name(arg): 
+def norm_file_name(arg):
     return arg.replace('/', os.sep).replace('\\', os.sep)
+
+
+def get_file_extension(file_name):
+    return os.path.splitext(file_name)[1]
+
+
+def load_file(file_name):
+    with open(norm_file_name(file_name)) as _:
+        return _.read()
+
+
+def get_file_content(file_name):
+    file_extension = get_file_extension(file_name)
+    if file_extension == '.json':
+        import json
+        return json.loads(load_file(file_name))
+    elif file_extension in ('.yml', '.yaml'):
+        import yaml
+        return yaml.load(load_file(file_name), Loader=yaml.FullLoader)
+    else:
+        raise ValueError('Unknown file extension')
