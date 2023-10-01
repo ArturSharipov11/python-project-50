7022b82f61bf713d4550ba1b30fdae02be1c414e
artur sharipov <sharipoffartur@yandex.ru>
2023-10-01 18:43:31 +0500

    f

--- gendiff/eng.py
+++ gendiff/eng.py
@@ -6,19 +6,25 @@ def get_comparison_resul(file1, file2):
     diff = {}
     for key in sorted(keys):
         if key not in file2:
-            diff[key] = {'type': REMOVED, 'value': convert_value(file1[key])}
+            diff[key] = {'type': REMOVED,
+                         'value': convert_value(file1[key])}
+
         elif key not in file1:
-            diff[key] = {'type': ADDED, 'value': convert_value(file2[key])}
-        else:
-            val1 = convert_value(file1[key])
-            val2 = convert_value(file2[key])
-            if isinstance(val1, dict) and isinstance(val2, dict):
-                diff[key] = {'type': NESTED, 'value':
-                             get_comparison_resul(val1, val2)}
-            elif val1 == val2:
-                diff[key] = {'type': UNCHANGED, 'value': val1}
-            else:
-                diff[key] = {'type': CHANGED, 'from': val1, 'to': val2}
+            diff[key] = {'type': ADDED,
+                         'value': convert_value(file2[key])}
+
+        elif isinstance(file1[key], dict) and isinstance(file2[key], dict):
+            diff[key] = {'type': NESTED,
+                         'value': get_comparison_resul(file1[key], file2[key])}
+
+        elif file1[key] == file2[key]:
+            diff[key] = {'type': UNCHANGED,
+                         'value': convert_value(file1[key])}
+
+        elif file1[key] != file2[key]:
+            diff[key] = {'type': CHANGED,
+                         'from': convert_value(file1[key]),
+                         'to': convert_value(file2[key])}
     return diff
 
 
