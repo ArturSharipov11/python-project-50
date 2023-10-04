9fe94e6aa5d1b94b57643052f76392ba69aa7e1a
artur sharipov <sharipoffartur@yandex.ru>
2023-10-04 09:54:12 +0500

    open_file

--- gendiff/main.py
+++ gendiff/main.py
@@ -1,5 +1,5 @@
 from gendiff.eng import get_comparison_resul
-from gendiff.parsing import parse
+from gendiff.parsing import parse, open_file
 from gendiff.formats.selection_format import format_select
 
 
@@ -11,10 +11,3 @@ def generate_diff(first_file, second_file, format_name='stylish'):
     found_diff = get_comparison_resul(first_dict, second_dict)
     selected_format = format_select(format_name)
     return selected_format(found_diff)
-
-
-def open_file(file_name):
-    with open(file_name, 'r') as file:
-        file_data = file.read()
-        file_format = file_name.split('.')[-1]
-        return file_data, file_format
--- gendiff/parsing.py
+++ gendiff/parsing.py
@@ -9,3 +9,10 @@ def parse(data, format):
         'yml': yaml.safe_load
     }
     return FORMATS.get(format)(data)
+
+
+def open_file(file_name):
+    with open(file_name, 'r') as file:
+        file_data = file.read()
+        file_format = file_name.split('.')[-1]
+        return file_data, file_format
